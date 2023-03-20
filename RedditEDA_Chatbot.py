d95ea67bcf4517938b0de587eb2cc44bdf9ec070
Daniel <85156415+KryptixOne@users.noreply.github.com>
2023-03-20 21:06:20 +0100

    EDA

--- RedditEDA_Chatbot.py
+++ RedditEDA_Chatbot.py
@@ -6,3 +6,31 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from transformers import pipeline
+
+
+def read_in_data(post_df_path: str,
+                 comments_df_path: str):
+    posts_df = pd.read_csv(post_df_path)
+    comments_df = pd.read_csv(comments_df_path)
+
+    return posts_df, comments_df
+
+def convert_utc_to_DateYear(posts_df):
+    posts_df['created_date'] = posts_df['created_utc'].apply(lambda x: dt.datetime.fromtimestamp(x))
+    posts_df['created_year'] = posts_df['created_date'].dt.year
+    return posts_df
+
+def merge_comments_posts(post_df,comments_df):
+    comments_posts_df = post_df.merge(comments_df, on='post_id', how ='left')
+    comments_posts_df = comments_posts_df[~comments_posts_df['comment'].isnull()]
+    return comments_posts_df
+
+if __name__ == "__main__":
+    path_posts = r'D:/ChatGPTAPI_Project/DS_ML_AI_posts.csv'
+    path_comments = r'D:/ChatGPTAPI_Project/DS_ML_AI_comments.csv'
+
+    posts_df, comments_df = read_in_data(path_posts, path_comments)
+    posts_df = convert_utc_to_DateYear(posts_df)
+    comments_posts_df = merge_comments_posts(posts_df,comments_df)
+
+    pass
--- main.py
+++ main.py
@@ -3,16 +3,14 @@ import pandas as pd
 
 
 def get_top_posts(subreddit_list='MachineLearning', limit=1000, time_filter='all'):
+    assert limit >= 1, 'Please enter a request of at least 1'
+
     reddit = praw.Reddit(client_id='thGxXn50aXKMCqWwz3boSg',
                          client_secret='2333NbmJYn-ksIZ77baMpQ9juMWQvg',
                          redirect_uri='http://localhost:8080',
                          user_agent='Kyrptix')
 
-    # pulls top submissions from all time in redditdev and learn python subreddits
-    """
-    for submission in reddit.subreddit('redditdev+learnpython').top(time_filter='all',limit = 1000):
-        print([submission])
-    """
+
     post_df = []
     posts = reddit.subreddit(subreddit_list).top(time_filter=time_filter, limit=limit)
     for post in posts:
@@ -31,19 +29,21 @@ def get_top_posts(subreddit_list='MachineLearning', limit=1000, time_filter='all
     return pd.DataFrame(post_df), reddit
 
 
-create_csv = True
-posts_df, reddit = get_top_posts(subreddit_list='MachineLearning+datascience+artificial', limit=10, time_filter='all')
-if create_csv == True:
-    posts_df.to_csv('DS_ML_AI_posts.csv', header=True, index=False)
+if __name__ == "__main__":
+    create_csv = True
+    posts_df, reddit = get_top_posts(subreddit_list='MachineLearning+datascience+artificial', limit=10, time_filter='all')
 
-comments_list = []
-for post_id in posts_df['post_id']:
-    submission = reddit.submission(post_id)
-    submission.comments.replace_more(limit=None)
-    for comment in submission.comments.list():
-        comments_list.append({'post_id': post_id,
-                              'comment': comment.body})
 
-comments_df = pd.DataFrame(comments_list)
-comments_df.to_csv('DS_ML_AI_comments.csv', header=True, index=False)
+    comments_list = []
+    for post_id in posts_df['post_id']:
+        submission = reddit.submission(post_id)
+        submission.comments.replace_more(limit=None)
+        for comment in submission.comments.list():
+            comments_list.append({'post_id': post_id,
+                                  'comment': comment.body})
 
+    comments_df = pd.DataFrame(comments_list)
+
+    if create_csv == True:
+        posts_df.to_csv('DS_ML_AI_posts.csv', header=True, index=False)
+        comments_df.to_csv('DS_ML_AI_comments.csv', header=True, index=False)
